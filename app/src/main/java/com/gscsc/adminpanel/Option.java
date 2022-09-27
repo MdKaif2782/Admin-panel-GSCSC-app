b02cc85d79ca276a2bf75f48bcc8d7eb5172905e
Md Kaif Ibn Zaman <mibnzaman02@gmail.com>
2022-09-27 17:27:27 +0600

    KOOL UI #19

--- app/src/main/java/com/gscsc/adminpanel/Option.java
+++ app/src/main/java/com/gscsc/adminpanel/Option.java
@@ -3,12 +3,17 @@ package com.gscsc.adminpanel;
 import android.content.Context;
 import android.content.Intent;
 import android.view.View;
+import android.view.ViewGroup;
+import android.widget.ImageView;
+import android.widget.LinearLayout;
 import androidx.appcompat.app.AppCompatActivity;
 import android.os.Bundle;
 
 public class Option extends AppCompatActivity {
     private Intent inviteIntent;
     private Context context;
+    private int screenHeight;
+    private ImageView admin_panel_image;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -16,6 +21,10 @@ public class Option extends AppCompatActivity {
         setContentView(R.layout.activity_option);
         context = this;
         inviteIntent = new Intent(this, Invite.class);
+        screenHeight = getResources().getDisplayMetrics().heightPixels;
+        admin_panel_image = findViewById(R.id.admin_panel_image);
+
+        resizeImage(screenHeight);
     }
     public void switchToInvite(View view) {
        context.startActivity(inviteIntent);
@@ -25,5 +34,13 @@ public class Option extends AppCompatActivity {
     public void onBackPressed() {
         super.onBackPressed();
         finish();
+    }
+    public void resizeImage(int height){
+        int newHeight = (int) (height * 0.34);
+        int imageWidth = admin_panel_image.getWidth();
+        int imageHeight = admin_panel_image.getHeight();
+        int newWidth = (int) (imageWidth * (newHeight / imageHeight));
+        LinearLayout.LayoutParams params = new LinearLayout.LayoutParams(newWidth, newHeight);
+        admin_panel_image.setLayoutParams(params);
     }
 }
