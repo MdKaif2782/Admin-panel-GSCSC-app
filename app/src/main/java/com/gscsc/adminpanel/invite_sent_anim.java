f256d6882d287d2157ed36654be0e3f5b97d8394
Md Kaif Ibn Zaman <mibnzaman02@gmail.com>
2022-10-01 15:36:47 +0600

    Invite animation added

--- app/src/main/java/com/gscsc/adminpanel/invite_sent_anim.java
+++ app/src/main/java/com/gscsc/adminpanel/invite_sent_anim.java
@@ -9,12 +9,16 @@ import com.airbnb.lottie.LottieAnimationView;
 
 public class invite_sent_anim extends AppCompatActivity {
     private LottieAnimationView animationView;
+    private TextView textView;
+    private TextView textView2;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_invite_sent_anim);
         animationView = findViewById(R.id.lottie_rocket);
+        textView = findViewById(R.id.invitation_sent_text);
+        textView2 = findViewById(R.id.awesome_text);
         //close after animation is done
         animationView.addAnimatorListener(new android.animation.Animator.AnimatorListener() {
             @Override
@@ -24,7 +28,15 @@ public class invite_sent_anim extends AppCompatActivity {
 
             @Override
             public void onAnimationEnd(android.animation.Animator animation) {
-                finish();
+                textView.setVisibility(android.view.View.VISIBLE);
+                textView2.setVisibility(android.view.View.VISIBLE);
+                //wait 1.5s
+                //add animation to text
+                textView2.setAnimation(android.view.animation.AnimationUtils.loadAnimation(invite_sent_anim.this, R.anim.invitation_sent_text_anim));
+                textView.setAnimation(android.view.animation.AnimationUtils.loadAnimation(getApplicationContext(),R.anim.invitation_sent_text_anim));
+                textView.postDelayed(() -> {
+                    finish();
+                }, 1500);
             }
 
             @Override
