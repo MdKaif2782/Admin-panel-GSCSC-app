98ddd94bb109dbcfefe3767ae2081200ad9b49fb
Md Kaif Ibn Zaman <mibnzaman02@gmail.com>
2022-10-03 20:12:56 +0600

    Rocket Fixing #69

--- app/src/main/java/com/gscsc/adminpanel/invite_sent_anim.java
+++ app/src/main/java/com/gscsc/adminpanel/invite_sent_anim.java
@@ -2,13 +2,17 @@ package com.gscsc.adminpanel;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.animation.Animator;
+import android.animation.AnimatorListenerAdapter;
 import android.os.Bundle;
+import android.util.Log;
 import android.widget.TextView;
 
 import com.airbnb.lottie.LottieAnimationView;
 
 public class invite_sent_anim extends AppCompatActivity {
     private LottieAnimationView animationView;
+    private LottieAnimationView animationView2;
     private TextView textView;
     private TextView textView2;
 
@@ -16,10 +20,25 @@ public class invite_sent_anim extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_invite_sent_anim);
-        animationView = findViewById(R.id.lottie_rocket);
+        animationView = findViewById(R.id.rocket_animation);
         textView = findViewById(R.id.invitation_sent_text);
         textView2 = findViewById(R.id.awesome_text);
-        //close after animation is done
+        animationView2 = findViewById(R.id.cloud_animation);
+
+        //rocket
+        animationView.setBackground(null);
+        animationView.setAnimation(R.raw.lottie_rocket_single);
+        animationView.setTranslationY(580);
+        animationView.playAnimation();
+
+
+        //cloud
+        animationView2.setBackground(null);
+        animationView2.setAnimation(R.raw.lottie_cloud);
+        animationView2.setTranslationY(500);
+        animationView2.playAnimation();
+
+
         animationView.addAnimatorListener(new android.animation.Animator.AnimatorListener() {
             @Override
             public void onAnimationStart(android.animation.Animator animation) {
