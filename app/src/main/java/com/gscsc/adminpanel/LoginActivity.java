b69f5b7d54fec931af0e154a185e4cb56432beeb
Md Kaif Ibn Zaman <mibnzaman02@gmail.com>
2022-10-05 16:25:41 +0600

    hopefully login animation done

--- app/src/main/java/com/gscsc/adminpanel/LoginActivity.java
+++ app/src/main/java/com/gscsc/adminpanel/LoginActivity.java
@@ -1,12 +1,19 @@
 package com.gscsc.adminpanel;
 
+import android.animation.Animator;
+import android.animation.AnimatorListenerAdapter;
 import android.content.Context;
 import android.content.Intent;
+import android.text.Editable;
+import android.text.TextWatcher;
 import android.util.Log;
 import android.view.View;
+import android.view.animation.AnimationUtils;
 import android.widget.*;
 import androidx.appcompat.app.AppCompatActivity;
 import android.os.Bundle;
+
+import androidx.constraintlayout.widget.ConstraintLayout;
 import androidx.swiperefreshlayout.widget.CircularProgressDrawable;
 
 import com.airbnb.lottie.LottieAnimationView;
@@ -21,11 +28,12 @@ public class LoginActivity extends AppCompatActivity {
     private TextInputLayout emailLayout, passwordLayout;
     private int screenWidth;
     private ImageView illustration;
-
     private View success;
     private View failure;
     private TextView login_text;
     private LottieAnimationView loading_anim;
+    private ConstraintLayout login_button_layout;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -37,6 +45,8 @@ public class LoginActivity extends AppCompatActivity {
         failure = findViewById(R.id.failure);
         login_text = findViewById(R.id.login_text);
         loading_anim = findViewById(R.id.loading_anim);
+        login_button_layout = findViewById(R.id.login_button_layout);
+        
 
         context = this;
         intent = new Intent(context, Option.class);;
@@ -48,16 +58,40 @@ public class LoginActivity extends AppCompatActivity {
             finish();
         }
         resizeImage(screenHeight);
+
+        email.addTextChangedListener(new TextWatcher() {
+            @Override
+            public void beforeTextChanged(CharSequence s, int start, int count, int after) {
+            }
+            @Override
+            public void onTextChanged(CharSequence s, int start, int before, int count) {
+                updateView();
+            }
+            @Override
+            public void afterTextChanged(Editable s) {
+            }
+        });
+
+        password.addTextChangedListener(new TextWatcher() {
+            @Override
+            public void beforeTextChanged(CharSequence s, int start, int count, int after) {
+            }
+            @Override
+            public void onTextChanged(CharSequence s, int start, int before, int count) {
+               updateView();
+            }
+            @Override
+            public void afterTextChanged(Editable s) {
+            }
+        });
     }
     public void login(View v){
-        CircularProgressDrawable circularProgressDrawable = new CircularProgressDrawable(context);
-        circularProgressDrawable.setStrokeWidth(4f);
-        circularProgressDrawable.setCenterRadius(35f);
-        circularProgressDrawable.setColorFilter(getResources().getColor(R.color.purple_500), android.graphics.PorterDuff.Mode.SRC_IN);
-        circularProgressDrawable.start();
-
-
-
+        Log.d("LoginActivity", "loginAnimation: Clicked");
+        login_text.setVisibility(View.GONE);
+        loading_anim.setVisibility(View.VISIBLE);
+        loading_anim.setAnimation(R.raw.loading);
+        loading_anim.setRepeatCount(0);
+        loading_anim.playAnimation();
         String emailText = email.getText().toString();
         String passwordText = password.getText().toString();
         if (emailText.isEmpty() || passwordText.isEmpty()) {
@@ -69,9 +103,35 @@ public class LoginActivity extends AppCompatActivity {
                 if (task.isSuccessful()) {
                     Toast.makeText(context, "Login Successful", Toast.LENGTH_SHORT).show();
                     System.out.println("Login Successful");
-                    context.startActivities(new Intent[]{intent});
+                    login_button_layout.setBackground(null);
+                    success.setVisibility(View.VISIBLE);
+                    success.setAnimation(AnimationUtils.loadAnimation(context, R.anim.text_alpha));
+                    loading_anim.cancelAnimation();
+                    loading_anim.setAnimation(R.raw.success);
+                    loading_anim.playAnimation();
+                    loading_anim.addAnimatorListener(new AnimatorListenerAdapter() {
+                        @Override
+                        public void onAnimationEnd(Animator animation) {
+                            super.onAnimationEnd(animation);
+                            //wait for 1 second
+                            try {
+                                Thread.sleep(1000);
+                            } catch (InterruptedException e) {
+                                e.printStackTrace();
+                            }
+                            context.startActivity(intent);
+                            finish();
+                        }
+                    });
                 } else {
                     Toast.makeText(context, "Login Failed", Toast.LENGTH_SHORT).show();
+                    login_button_layout.setBackground(null);
+                    login_button_layout.setClickable(false);
+                    failure.setVisibility(View.VISIBLE);
+                    failure.setAnimation(AnimationUtils.loadAnimation(context, R.anim.text_alpha));
+                    loading_anim.cancelAnimation();
+                    loading_anim.setAnimation(R.raw.failed);
+                    loading_anim.playAnimation();
                     System.out.println("Login Failed");
                 }
             });
@@ -97,27 +157,46 @@ public class LoginActivity extends AppCompatActivity {
 
 
     public void loginAnimation(View view) {
+        Boolean isLogin = false;
         //wait for 3 second
         Log.d("LoginActivity", "loginAnimation: Clicked");
         login_text.setVisibility(View.GONE);
         loading_anim.setVisibility(View.VISIBLE);
         loading_anim.setAnimation(R.raw.loading);
+        loading_anim.setRepeatCount(0);
         loading_anim.playAnimation();
         //wait for 3 second
         new android.os.Handler().postDelayed(
                 new Runnable() {
                     public void run() {
+                        if(isLogin){
+                            login_button_layout.setBackground(null);
+                            success.setVisibility(View.VISIBLE);
+                            success.setAnimation(AnimationUtils.loadAnimation(context, R.anim.text_alpha));
+                            loading_anim.cancelAnimation();
+                            loading_anim.setAnimation(R.raw.success);
+                            loading_anim.playAnimation();
+                        }else {
+                            login_button_layout.setBackground(null);
+                            login_button_layout.setClickable(false);
+                            failure.setVisibility(View.VISIBLE);
+                            failure.setAnimation(AnimationUtils.loadAnimation(context, R.anim.text_alpha));
+                            loading_anim.cancelAnimation();
+                            loading_anim.setAnimation(R.raw.failed);
+                            loading_anim.playAnimation();
+                        }
 
-                        success.setVisibility(View.VISIBLE);
-                        loading_anim.cancelAnimation();
-                        loading_anim.setAnimation(R.raw.success);
-                        loading_anim.playAnimation();
                     }
                 },
                 3000);
-
-
-
-
+    }
+    public void updateView() {
+        loading_anim.cancelAnimation();
+        loading_anim.setVisibility(View.GONE);
+        login_text.setVisibility(View.VISIBLE);
+        login_button_layout.setBackground(getResources().getDrawable(R.drawable.rounded_corners));
+        login_button_layout.setClickable(true);
+        success.setVisibility(View.GONE);
+        failure.setVisibility(View.GONE);
     }
 }
