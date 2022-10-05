c05387b43660ff0337ca2f898a6fb637f3035680
Md Kaif Ibn Zaman <mibnzaman02@gmail.com>
2022-10-05 13:24:35 +0600

    What I'm doing with my life

--- app/src/main/java/com/gscsc/adminpanel/invite_sent_anim.java
+++ app/src/main/java/com/gscsc/adminpanel/invite_sent_anim.java
@@ -22,47 +22,19 @@ public class invite_sent_anim extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_invite_sent_anim);
-        animationView = findViewById(R.id.rocket_animation);
+        animationView = findViewById(R.id.paper_rocket_animation);
         textView = findViewById(R.id.invitation_sent_text);
         textView2 = findViewById(R.id.awesome_text);
-        animationView2 = findViewById(R.id.cloud_animation);
 
-        //rocket
-        animationView.setBackground(null);
-        animationView.setAnimation(R.raw.lottie_rocket_single);
-        animationView.setTranslationY(580);
-        animationView.playAnimation();
-
-
-        //cloud
-        animationView2.setBackground(null);
-        animationView2.setAnimation(R.raw.lottie_cloud);
-        animationView2.setTranslationY(500);
-        animationView2.playAnimation();
 
 
         animationView.addAnimatorListener(new android.animation.Animator.AnimatorListener() {
             @Override
             public void onAnimationStart(android.animation.Animator animation) {
-                //wait 1000ms for thread
-                try {
-                    Thread.sleep(1000);
-                    View view = findViewById(R.id.rocket_animation);
-                    view.setAnimation(android.view.animation.AnimationUtils.loadAnimation(getApplicationContext(), R.anim.rocket_goes_up_anim));
-                } catch (InterruptedException e) {
-                    e.printStackTrace();
-                }
-
             }
 
             @Override
             public void onAnimationEnd(android.animation.Animator animation) {
-                //wait 1000ms for thread
-                try {
-                    Thread.sleep(2000);
-                } catch (InterruptedException e) {
-                    e.printStackTrace();
-                }
                 textView.setVisibility(android.view.View.VISIBLE);
                 textView2.setVisibility(android.view.View.VISIBLE);
                 //wait 1.5s
